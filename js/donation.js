1e0929d337aca2131d6d4e462f481213a9e66c6b
mohammadalamin600 <68004229+mohammadalamin600@users.noreply.github.com>
2024-09-23 22:48:54 +0600

    i got the all of input and inner text as number

---
+++ js/donation.js
@@ -0,0 +1,10 @@
+const targetBalance = getInnerText('target-balance');
+
+const noakhaliDonation = getInnerText('noakhali-donation');
+const noakhaliDonationAmount = getInputValue('noakhali-donation-amount');
+
+const feniDonation = getInnerText('feni-donation');
+const feniDonationAmount = getInputValue('feni-donation-amount');
+
+const quotaDonation = getInnerText('quota-donation');
+const quotaDonationAmount = getInputValue('quota-donation-amount');
